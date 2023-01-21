0e9b2354cc2007f55ad3317cf670373febb48e5e
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-01-22 00:49:27 +0530

    Add: Calculating Wages till Number of Working Days or
    Total Working Hours per month is Reached

--- empWageComputationProblem.sh
+++ empWageComputationProblem.sh
@@ -2,26 +2,34 @@ echo "Welcome To Employee Wage Computation Problem"
 isPartTime=1;
 isFullTime=2;
 totalSalary=0;
+empHrs=0;
+totalWorkingHour=20;
 empRatePerHr=20;
 numWorkingDays=20;
-
-for (( day=1; day<=$numWorkingDays; day++ ))
+day=1;
+while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
 do
+randomCheck=$((RANDOM%3));
 
-   empCheck=$((RANDOM%3));
-       case $empCheck in
-                $isFullTime)
-                        empHrs=8
-                         ;;
-                $isPartTime)
-                        empHrs=4
-                         ;;
-                 *)
-                 empHrs=0
-                         ;;
-      esac
-      
-      salary=$(($empHrs*$empRatePerHr));
-      totalSalary=$(($totalSalary+$salary));
+case $randomCheck in $isFullTime)
+
+                      empHrs=8;;
+                       $isPartTime)
+                       empHrs=4;;
+                         *)
+                        empHrs=0;;
+ esac
+   totalWorkingHour=$(($totalWorkingHour+$empHrs))
+if [ $totalWorkingHour -gt 100 ]
+
+then
+totalWorkingHour=$(($totalWorkingHour-$empHrs))
+break;
+fi
+salary=$(($empRatePerHr*$empHrs))
+
+totalSalary=$(($totalSalary+$salary))
+((day++))
 done
+echo "Employee has earned $totalSalary$ in a month (Total working HOur :$totalWorkingHour)";
 
